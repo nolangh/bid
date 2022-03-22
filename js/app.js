7a9372ba2ea6143ff7e3d13af23d6b564df3e7b2
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-03-22 10:14:41 -0600

    -a

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-const leftValue = document.querySelector("left-input").value;
+/* ---------------------------- ANCHOR Selectors ---------------------------- */:w
+
+const leftValue = document.querySelector("left-input");
+const rightValue = document.querySelector("right-input");
+const mainDisplay = document.querySelector("btm-display");
+
+/* ------------------------------ ANCHOR Events ----------------------------- */
+
+leftValue.addEventListener("keyress", leftSubmit)
+rightValue.addEventListener("keypress", rightSubmit)
+
+
+
