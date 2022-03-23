3b5f923af6aa35889e135247bd8a5f7f00da82ea
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-03-23 08:01:18 -0600

    verrw

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ---------------------------- ANCHOR Selectors ---------------------------- */:w
 
 const leftValue = document.querySelector("left-input");
@@ -7,31 +6,13 @@ const mainDisplay = document.querySelector("btm-display");
 
 /* ------------------------------ ANCHOR Events ----------------------------- */
 
-leftValue.addEventListener("keyress", leftSubmit)
-rightValue.addEventListener("keypress", rightSubmit)
+leftValue.addEventListener("keydown", leftSubmit)
+rightValue.addEventListener("keydown", rightSubmit)
 
-
-
-=======
-const leftInput = document.querySelector("left-input");
-const rightInput = document.querySelector("right-input");
-const btmDisplay = document.querySelector("btm-display");
-const leftBtn = document.querySelector("left-button");
-const rightBtn = document.querySelector("right-button");
-
-/* ------------------------------ ANCHOR Events ----------------------------- */
-
-leftBtn.addEventListener("click", submitLeft);
-rightBtn.addEventListener("click", submitRight);
-
-/* ------------------------- ANCHOR Display the bids ------------------------ */
-function submitLeft() {
-	const leftBidOutPut = document.createElement("p");
-	leftBidOutPut.innerText = leftInput.value;
-	leftBidOutPut.classList.add("output-h1");
-
-	btmDisplay.appendChild(leftBidOutPut);
-
-	leftInput.value = "";
+function leftSubmit(e){
+    let target = e.target;
+    
 }
->>>>>>> ed1ef09df1d25f8aae250d37633114eea934ac98
+
+
+
