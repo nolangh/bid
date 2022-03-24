5198b08a53471ecb32c9c84ca61478256a2c9661
nolangh <nolanghiggins@gmail.com>
2022-03-24 16:43:33 -0600

    -a

--- js/app.js
+++ js/app.js
@@ -13,17 +13,11 @@ leftSubmitBtn.addEventListener("click", addLeftBid);
 rightSubmitButton.addEventListener("click", addRightBid);
 
 function addLeftBid() {
-	//Creates left value h1
 	const leftBidDivH1 = document.createElement("h1");
-	leftBidDivH1.innerText = leftValue.value;
+	leftBidDivH1.innertext = leftValue.value;
 	leftBidDivH1.classList.add("left-bid-h1");
-
-	//Adds h1 to left display div
 	leftBtmDiv.appendChild(leftBidDivH1);
-
-	//Adds everything to Left display
-	//mainDisplay.appendChild(leftBidDiv);
-
+	saveLeft(leftValue.value);
 	leftValue.value = "";
 }
 
@@ -31,8 +25,31 @@ function addRightBid() {
 	const rightBidH1 = document.createElement("h1");
 	rightBidH1.innerText = rightValue.value;
 	rightBidH1.classList.add("right-bid-h1");
-
 	rightBtmDiv.appendChild(rightBidH1);
-
+	saveRight(rightValue.value);
 	rightValue.value = "";
+}
+
+function saveLeft(leftBidValue) {
+	let bids;
+	if (localStorage.getItem("leftBidValue") === null) {
+		bids = [];
+	} else {
+		bids = JSON.parse(localStorage.getItem("leftBidValue"));
+	}
+
+	bids.push(leftBidValue);
+	localStorage.setItem("leftBidValue", JSON.stringify(bids));
+}
+
+function saveRight(rightValue) {
+	let rBid;
+	if (localStorage.getItem("rightBid") === null) {
+		rBid = [];
+	} else {
+		rBid = JSON.parse(localStorage.getItem("rightBid"));
+	}
+
+	rBid.push(rightValue);
+	localStorage.setItem("rightbid", JSON.stringify(rBid));
 }
